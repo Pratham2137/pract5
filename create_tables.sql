1aeeaea36011d867918f2dfe9833e81390a645fd
Pratham02 <prathamshinde2137@gmail.com>
2025-07-04 02:30:19 +0530

    sql update

--- create_tables.sql
+++ create_tables.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE IF NOT EXISTS demo;
 USE demo;
 CREATE TABLE students (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100),
-    marks INT,
+ id INT PRIMARY KEY AUTO_INCREMENT,
+ name VARCHAR(100),
+ marks INT
 );
 INSERT INTO students (name,marks) VALUES
 ("Pratham", 90),
