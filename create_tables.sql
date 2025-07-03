b358a34b21641df32a0fc0d791418b6dcccb0422
Pratham02 <prathamshinde2137@gmail.com>
2025-07-04 02:27:09 +0530

    first commit

---
+++ create_tables.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS demo;
+USE demo;
+CREATE TABLE students (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100),
+    marks INT,
+);
+INSERT INTO students (name,marks) VALUES
+("Pratham", 90),
+("Demo", 85),
+("TastTest",75);
